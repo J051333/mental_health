9903bd99cc9d9d3091a505107d9a7277a974410c
Josie Scarlett Whitlock <62673737+J051333@users.noreply.github.com>
2022-03-08 21:11:47 -0600

    Created base TrackerMenu class
    Created base TrackerMenu class and renamed the file

---
+++ lib/day_tracker/tracker_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TrackerMenu extends StatefulWidget {
+  const TrackerMenu({Key? key}) : super(key: key);
+
+  @override
+  _TrackerMenuState createState() => _TrackerMenuState();
+}
+
+class _TrackerMenuState extends State<TrackerMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
